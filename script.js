cad99c5b7c4317cb4d11baa4951bffe1c6118059
Damediaz <diaz.damian2015@gmail.com>
2021-10-30 13:01:50 -0700

    updated script.js and css

--- script.js
+++ script.js
@@ -21,29 +21,51 @@ let questions = [
     },
 
     {
-        question: '?',
-        choice1:'2',
-        choice2:'4',
-        choice3:'21',
-        choice4:'17',
+        question: 'What tag is used to define – and place – an interactive button in an HTML document??',
+        choice1:'<clickfield>',
+        choice2:'<footer>',
+        choice3:'<td>',
+        choice4:'<button>',
+        answer: 4,
+    },
+
+    {
+        question: 'What tag defines a division or the beginning/end of an individual section in an HTML document?',
+        choice1:'<div>',
+        choice2:'<meta>',
+        choice3:'<img>',
+        choice4:'<table>',
         answer: 1,
     },
 
     {
-        question: 'What is 2 + 2?',
-        choice1:'2',
-        choice2:'4',
-        choice3:'21',
-        choice4:'17',
-        answer: 1,
-    },
-
-    {
-        question: 'What is 2 + 2?',
-        choice1:'2',
-        choice2:'4',
-        choice3:'21',
-        choice4:'17',
-        answer: 1,
+        question: 'What tag is used to render or transform text into an important (bold) version?',
+        choice1:'<blockquote>',
+        choice2:'<strong>',
+        choice3:'<a>',
+        choice4:'<em>',
+        answer: 2,
     },
 ]
+
+var SCORE_POINTS = 100
+var MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+
+}
