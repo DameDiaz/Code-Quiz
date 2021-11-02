5a20796fa3449131764b514f938cf9026b5ee470
Damediaz <diaz.damian2015@gmail.com>
2021-11-02 13:40:04 -0700

    updated everything

--- end.js
+++ end.js
@@ -3,16 +3,16 @@ var saveScoreBtn = document.querySelector('#saveScoreBtn')
 var finalScore = document.querySelector('#finalScore')
 var mostRecentScore = document.querySelector('#mostRecentScore')
 
-var highScores = JSON.parsel(localStorage.getItem('highScores')) || []
+var highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 var MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
-})
+// username.addEventListener('keyup', () => {
+//     saveScoreBtn.disabled = !username.value
+// })
 
 saveHighScore = e => {
     e.preventDefault()
--- highscore.js
+++ highscore.js
@@ -5,3 +5,8 @@ highScoresList.innerHTML =
 highScores.map(score =>{
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
+
+var saveBtn = document.querySelector('#saveScoreBtn')
+saveBtn.addEventListener('click', () => {
+    console.log('hi')
+})
